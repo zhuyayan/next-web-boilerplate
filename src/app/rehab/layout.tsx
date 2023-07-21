2d994455ad4016e87f3173c74706caf082985345
zhuyayan <1606095629@qq.com>
2023-07-21 17:18:52 +0800

    临时提交

--- src/app/rehab/layout.tsx
+++ src/app/rehab/layout.tsx
@@ -4,20 +4,21 @@ import NavBar from "@/components/layout/NavBar";
 import {ReduxProvider} from "@/redux/provider";
 import {ApiProvider} from "@reduxjs/toolkit/query/react";
 import {rehabApi} from "@/redux/features/rehab/rehab-slice";
+import FullScreenContainer from "@/components/layout/FullScreen";
 
 export default function RehabLayout({children}:{children:React.ReactNode}){
     return (
         <section>
             <ApiProvider api={rehabApi}>
             <ReduxProvider>
-
-                    <div className="flex flex-col sm:flex-row">
-                        <div className="w-full bg-gray-100 sm:h-screen">
-                            <NavBar />
-                            <div>{children}</div>
-                        </div>
-                    </div>
-
+              <FullScreenContainer>
+                <div className="flex flex-col sm:flex-row">
+                  <div className="w-full bg-gray-100 sm:h-screen">
+                    <NavBar />
+                    <div>{children}</div>
+                  </div>
+                </div>
+              </FullScreenContainer>
             </ReduxProvider>
             </ApiProvider>
         </section>
---
+++ src/components/layout/FullScreen.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+import React from "react";
+
+const StyledFullScreenContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: #f0f0f0; /* 可以根据您的需求设置背景颜色 */
+  /* 添加其他样式，根据需要进行调整 */
+`;
+
+export default function FullScreenContainer({children}:{children:React.ReactNode}){
+  return (
+      <>
+        <StyledFullScreenContainer>
+          {children}
+        </StyledFullScreenContainer>
+      </>
+  )
+}
