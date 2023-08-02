06fd4009b95b143466efc15a65fa853fbbbde7ac
zhuyayan <1606095629@qq.com>
2023-08-02 10:32:51 +0800

    固定line高度

--- src/components/rehab/prescription/PrescriptionLine.tsx
+++ src/components/rehab/prescription/PrescriptionLine.tsx
@@ -12,7 +12,7 @@ export default function PrescriptionLine(params: {trainData:RealTimeTrainData[]}
       <LineChart width={500} height={300} data={params.trainData} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis dataKey="name" />
-        {/*<YAxis domain={[0, 3000]}/>*/}
+        <YAxis domain={[0, 3000]}/>
         <Tooltip />
         <Legend />
         <Line type="monotone" dataKey="D" stroke="#8884d8" activeDot={{ r: 8 }} />
